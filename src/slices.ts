04e46d58eb136dc38093f1db72dfec744a76621b
roggc <roggc9@gmail.com>
2023-06-27 15:12:36 +0200

    use rcs@9.0.8

--- src/slices.ts
+++ src/slices.ts
@@ -4,28 +4,30 @@ import getHookAndProviderFromSlices, {
 import { ImageConfig, GuestSession, Rating } from "./types";
 
 export const { useSlice, Provider } = getHookAndProviderFromSlices({
-  guestSession: defineSlice<GuestSession | null>({
-    initialArg: null,
-    init: () => null,
-    isGetInitialStateFromStorage: true,
-  }),
-  imageConfig: defineSlice<ImageConfig | null>({
-    initialArg: null,
-    middleware: [
-      () => (next) => (action) => {
-        console.log("in the middleware - imageConfig", action);
-        return next(action);
-      },
-    ],
-  }),
-  ratings: defineSlice<Rating[]>({
-    initialArg: [],
-    middleware: [
-      () => (next) => (action) => {
-        console.log("in the middleware - ratings", action);
-        return next(action);
-      },
-    ],
-    isGetInitialStateFromStorage: true,
-  }),
+  slices: {
+    guestSession: defineSlice<GuestSession | null>({
+      initialArg: null,
+      init: () => null,
+      isGetInitialStateFromStorage: true,
+    }),
+    imageConfig: defineSlice<ImageConfig | null>({
+      initialArg: null,
+      middleware: [
+        () => (next) => (action) => {
+          console.log("in the middleware - imageConfig", action);
+          return next(action);
+        },
+      ],
+    }),
+    ratings: defineSlice<Rating[]>({
+      initialArg: [],
+      middleware: [
+        () => (next) => (action) => {
+          console.log("in the middleware - ratings", action);
+          return next(action);
+        },
+      ],
+      isGetInitialStateFromStorage: true,
+    }),
+  },
 });
